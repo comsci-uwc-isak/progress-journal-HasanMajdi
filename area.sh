fcf4de3fb88fccc1259c0b5e6d990a68b78d38e7
M19-008 <m19-008@M19-008s.local>
2019-09-04 10:41:32 +0900

    first time using commit

---
+++ area.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#This program shows the area of a rectangle 
+#The user provides the dimentions 
+
+echo "Enter the width of the rectangle. press ENTER:"
+read width 
+
+echo "Enter the length. Press ENTER:"
+read length 
+
+((area=$width*$length))
+
+echo -n "The area of the rectangle is "
+echo $area
---
+++ check.sh
@@ -0,0 +1,14 @@
+#!bin/bash
+
+#This program checks that a number entered by the user is within [0, 5]
+
+echo "Enter the grade and press ENTER:"
+read num 
+
+#checks the number
+if [ $num -ge 0 ]; then
+                      echo "number is greater than zero"
+else
+                     echo "number is not greater than zero" 
+fi
+
